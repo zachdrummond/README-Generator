ea68833ab1c8bef0a820832cfb8046cd10b75e81
Zach Drummond <zachdrummond3@gmail.com>
2020-09-23 15:07:05 -0400

    Installed packages

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
 
+    // Prompts the user
+    // Once you get the answers, you call generateMarkdown with require, pass in the object
+    
 }
 
 // function call to initialize program
